199bdaaf115543e152f0418bc8dc94112fb15b8d
Ludovic Beauregard Barwell <lbeau115@uottawa.ca>
2025-04-03 00:13:24 -0400

    Added date format support for search filters

--- client/src/components/SearchView.tsx
+++ client/src/components/SearchView.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 const SearchView = () => {
+  const [startDate, setStartDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
+  const [endDate, setEndDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
+
   return (
     <form style={{ margin: "1em", borderBottom: "solid lightgray" }}>
       <div className="container">
@@ -7,20 +16,56 @@ const SearchView = () => {
             <label htmlFor="destination" className="form-label">
               Destination
             </label>
-            <input type="text" className="form-control" id="destination" />
+            <input
+              type="text"
+              className="form-control"
+              id="destination"
+              placeholder="New York"
+            />
           </div>
-          <div className="col mb-3">
-            <label htmlFor="dates" className="form-label">
-              Dates
+
+          <div className="col">
+            <label htmlFor="endDateField" className="form-label">
+              Start date
             </label>
-            <input type="text" className="form-control" id="dates" />
+            <input
+              type="date"
+              className="form-control"
+              id="endDateField"
+              defaultValue={startDate}
+              min={new Date().toISOString().split("T")[0]}
+              max={endDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
           </div>
+
+          <div className="col">
+            <label htmlFor="endDateField" className="form-label">
+              End date
+            </label>
+            <input
+              type="date"
+              className="form-control"
+              id="endDateField"
+              min={startDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+
           <div className="col mb-3">
             <label htmlFor="travellers" className="form-label">
               Travellers
             </label>
-            <input type="text" className="form-control" id="travellers" />
+            <input
+              type="number"
+              className="form-control"
+              id="travellers"
+              min={1}
+              max={20}
+              defaultValue="1"
+            />
           </div>
+
           <button
             type="submit"
             className="col btn btn-primary"
